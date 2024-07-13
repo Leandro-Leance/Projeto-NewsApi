8f3bfbe2b804ebf0ce9c67970a3d986f81a1c354
Leandro <leandroleance.26@gmail.com>
2024-07-13 12:24:19 -0300

    Projeto-NewsApi

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Header } from "../components/header";
+import { Separator } from "@radix-ui/react-separator";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <Separator />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header/index.jsx
@@ -0,0 +1,11 @@
+import { Separator } from "@radix-ui/react-separator";
+
+export function Header() {
+  return (
+    <div className="shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] border-b-1 border-slate-950 w-full p-16 bg-slate-300">
+      <h1 className=" shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] border-b-1 border-slate-950 text-7xl xl:text-8xl font-bold text-center p-16">
+        Notícias
+      </h1>
+    </div>
+  );
+}
